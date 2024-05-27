390644d2b912d80d809756902bc5b603bfdd40a7
irina <irina.potashevskaya@gmail.com>
2024-05-27 06:37:56 +0300

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ import { footerLists } from "../constants";
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
-    <div className={`flex justify-between md:flex-row flex-col mb-8 w-full`}>
+    <div className={`flex justify-between ss:flex-row flex-col mb-8 w-full`}>
       <div className="flex-1 flex flex-col justify-start mr-10">
         <img src={logoTZ} alt="trizons" className="w-[100px] object-contain mb-2" />
         <p className={`${styles.paragraph} mt4 max-w-[310px]`}> Выполняем поставленные задачи. Оправдываем доверие наших заказчиков.</p>
       </div>
 
-      <div className="flex-[1.5] w-full flex flex-row justify-end flex-wrap md:mt-0 mt-10">
+      <div className="flex-[1.5] w-full flex flex-row ss:justify-end flex-wrap ss:mt-0 mt-10">
         {footerLists.map((footerList) => (
           <div key={footerList.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
